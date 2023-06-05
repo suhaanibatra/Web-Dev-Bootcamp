2c58d382d9db7428c7522f2500f441ba792e5391
Suhaani Batra <95175688+suhaanibatra@users.noreply.github.com>
2023-06-05 17:06:00 +0530

    Add files via upload

---
+++ React-complete-learning/lec_12_keeper-app-part-1/src/components/Footer.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Footer() {
+  const currYear = new Date().getFullYear();
+  return (
+    <footer>
+      {" "}
+      <p>Copyright ⓒ {currYear}</p>{" "}
+    </footer>
+  );
+}
+
+export default Footer;
---
+++ React-complete-learning/lec_20_keeper-app-part-2/src/components/App.js
@@ -0,0 +1,19 @@
+import React from "react";
+import Header from "./Header";
+import Footer from "./Footer";
+import Note from "./Note";
+import notes from "../notes";
+
+function App() {
+  return (
+    <div>
+      <Header />
+      {notes.map((note) => (
+        <Note title={note.title} content={note.content} key={note.id} />
+      ))}
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
